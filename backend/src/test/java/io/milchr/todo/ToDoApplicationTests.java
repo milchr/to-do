f5a208d416b973fbbcf6be756b1986bbea19b50e
milchr <milchr96@gmail.com>
2024-01-14 17:01:52 +0100

    Initial commit

---
+++ backend/src/test/java/io/milchr/todo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.milchr.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
